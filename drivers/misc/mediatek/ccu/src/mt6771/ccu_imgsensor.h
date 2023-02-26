e1cd2ba64dc9fe379c84b0dc70877ea85ee80526
rumplestilzken <rumplestilzken@gmail.com>
2023-02-25 21:40:35 -0500

    Fix issue with ccu_i2c.h

--- drivers/misc/mediatek/ccu/src/mt6771/ccu_imgsensor.h
+++ drivers/misc/mediatek/ccu/src/mt6771/ccu_imgsensor.h
@@ -13,7 +13,7 @@
 
 #ifndef _CCU_IMGSENSOR_H_
 #define _CCU_IMGSENSOR_H_
-#include "ccu_i2c.h"
+#include <ccu_i2c.h>
 void ccu_get_current_fps(int32_t *current_fps_list);
 
 void ccu_get_sensor_i2c_info(struct ccu_i2c_info *sensor_info);
