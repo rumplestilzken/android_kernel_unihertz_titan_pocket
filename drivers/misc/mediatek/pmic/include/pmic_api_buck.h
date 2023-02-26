d0bd70b8cdfead396798caf5001c39e9397df8e0
rumplestilzken <rumplestilzken@gmail.com>
2023-02-26 08:50:07 -0500

    Updated code, makefile and configuration files for CONFIG_MTK_PMIC_CHIP_MT6771

--- drivers/misc/mediatek/pmic/include/pmic_api_buck.h
+++ drivers/misc/mediatek/pmic/include/pmic_api_buck.h
@@ -50,8 +50,7 @@
 #include "mt6390/mtk_pmic_api_buck.h"
 #endif
 
-// #TODO CONFIG_MTK_PMIC_CHIP_MT6771
-#ifdef CONFIG_MACH_MT6771
+#ifdef CONFIG_MTK_PMIC_CHIP_MT6771
 #include "mt6771/mtk_pmic_api_buck.h"
 #endif
 #endif				/* _PMIC_API_BUCK_H_ */
